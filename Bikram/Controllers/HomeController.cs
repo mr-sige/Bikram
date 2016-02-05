a1869938f6f09b347150f61ec6981bac19a50aaf
sige <gsinka@logmein.com>
2016-02-05 12:57:47 +0100

    cleanup solution

--- Bikram/Controllers/HomeController.cs
+++ Bikram/Controllers/HomeController.cs
@@ -14,19 +14,5 @@ namespace Bikram.Controllers
             var result = Bikram.DoTheFckinWork();
             return View(new BikramViewModel(result));
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
--- Bikram/Startup.cs
+++ Bikram/Startup.cs
@@ -8,7 +8,7 @@ namespace Bikram
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            
         }
     }
 }
