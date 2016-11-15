5ba189d8bf1f40f5c025fc2f50c52d6f0430bf17
sige <gsinka@logmein.com>
2016-11-15 10:05:01 +0000

    cleanup

--- Bikram/Controllers/Bikram.cs
+++ Bikram/Controllers/Bikram.cs
@@ -1,81 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
-using System.Web;
 using System.Net;
 using Antlr.Runtime.Misc;
 using HtmlAgilityPack;
 
 namespace Bikram.Controllers
 {
-	public class Bikram
-	{
-		public static List<string> GetTodaysClasses()
-		{
+    public class Bikram
+    {
+        public static List<string> GetTodaysClasses()
+        {
+            return GetClassesForDay(GetNumberOfToday());
+        }
 
-			return GetClassesForDay(GetNumberOfToday());
-		}
+        public static List<string> GetTomorrowsClasses()
+        {
+            int today = GetNumberOfToday();
+            int tomorrow = today + 1;
+            if (tomorrow == 8)
+            {
+                tomorrow = 1;
+            }
 
-		public static List<string> GetTomorrowsClasses()
-		{
-			int today = GetNumberOfToday();
-			int tomorrow = today + 1;
-			if (tomorrow == 8)
-			{
-				tomorrow = 1;
-			}
+            return GetClassesForDay(tomorrow);
+        }
 
-			return GetClassesForDay(tomorrow);
-		}
+        private static int GetNumberOfToday()
+        {
+            int today = (int)DateTime.Today.DayOfWeek;
 
-		static int GetNumberOfToday()
-		{
-			int today = (int)DateTime.Today.DayOfWeek;
+            if (today.Equals(0))
+            {
+                today = today + 7; // because Sunday is the first day
+            }
 
-			if (today.Equals(0))
-			{
-				today = today + 7; //because Sunday is the first day
-			}
-			return today;
-		}
+            return today;
+        }
 
-		static List<string> GetClassesForDay(int numOfDay)
-		{
-			HtmlNode table;
-			using (WebClient client = new WebClient())
-			{
-				HtmlDocument html = new HtmlDocument();
-				html.LoadHtml(new WebClient().DownloadString("http://bikram.hu/?p=orarend"));
-				table = html.DocumentNode.SelectSingleNode("//*[@id=\"ora\"]");
-			}
+        private static List<string> GetClassesForDay(int numOfDay)
+        {
+            HtmlNode table;
+            using (WebClient client = new WebClient())
+            {
+                HtmlDocument html = new HtmlDocument();
+                html.LoadHtml(client.DownloadString("http://bikram.hu/?p=orarend"));
+                table = html.DocumentNode.SelectSingleNode("//*[@id=\"ora\"]");
+            }
 
+            HtmlNodeCollection startTimes = table.SelectNodes(".//tr/td[1]");
+            HtmlNodeCollection todayColumn = table.SelectNodes(".//tr/td[" + (numOfDay + 1) + "]");
 
-			var startTimes = table.SelectNodes(".//tr/td[1]");
-			var todayColumn = table.SelectNodes(".//tr/td[" + (numOfDay + 1) + "]");
+            List<string> startList = new List<string>();
+            startList.AddRange(startTimes.Select(n => n.InnerText));
 
-			List<string> startList = new List<string>();
-			startList.AddRange(startTimes.Select(n => n.InnerText));
+            List<string> todayList = new List<string>();
+            todayList.AddRange(todayColumn.Select(n => n.InnerText));
 
-			List<string> todayList = new List<string>();
-			todayList.AddRange(todayColumn.Select(n => n.InnerText));
+            List<string> result = new ListStack<string>();
 
-			List<string> result = new ListStack<string>();
+            for (int i = 0; i < startList.Count; i++)
+            {
+                startList[i] = startList[i].Replace(".", ":");
+            }
 
-			for (int i = 0; i < startList.Count; i++)
-			{
-				startList[i] = startList[i].Replace(".", ":");
-			}
+            for (int i = 0; i < todayList.Count; i++)
+            {
+                if (todayList[i] != "&nbsp;")
+                {
+                    result.Add(string.Concat(startList[i], " ", todayList[i]));
+                }
+            }
 
-			for (int i = 0; i < todayList.Count; i++)
-			{
-				if (todayList[i] != "&nbsp;")
-				{
-					result.Add(String.Concat(startList[i], " ", todayList[i]));
-				}
-			}
-
-			return result;
-		}
-	}
+            return result;
+        }
+    }
 }
--- Bikram/Controllers/HomeController.cs
+++ Bikram/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using Bikram.Models;
 
@@ -10,10 +7,10 @@ namespace Bikram.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-		{
-			List<string> today = Bikram.GetTodaysClasses();
-			List<string> tomorrow = Bikram.GetTomorrowsClasses(); 
-			return View(new BikramViewModel(today, tomorrow));
+        {
+            List<string> today = Bikram.GetTodaysClasses();
+            List<string> tomorrow = Bikram.GetTomorrowsClasses(); 
+            return this.View(new BikramViewModel(today, tomorrow));
         }
     }
 }
